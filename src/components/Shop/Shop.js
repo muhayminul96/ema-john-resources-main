346355fc99543e68b6b554b86b9fea7c28767d4e
muhayminul <muhayminul96@gmail.com>
2022-03-25 16:11:26 +0600

    i am try to commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            
+            <div className="product-container">
+                <h2>product part</h2>
+            </div>
+            <div className="order-container">
+                <h2>order Part</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
